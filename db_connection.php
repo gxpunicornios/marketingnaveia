71929f57f2033670bbd99efb1489998ab008e5ee
Henrique <henrique.carrasco.1@gmail.com>
2017-07-28 14:49:04 -0300

    post insert updated

--- db_connection.php
+++ db_connection.php
@@ -179,7 +179,7 @@ class DbConnect {
 		}
 
 		$date = date('Y-m-d H:i:s');
-		$sql = "INSERT INTO post VALUES (DEFAULT,'$post_title','$post_subtitle','$post_description','$post_author','$date','','$post_lp_id','$post_preview')";
+		$sql = "INSERT INTO post VALUES (DEFAULT,'$post_title','$post_subtitle','$post_description','$post_author','$date','','$post_lp_id','$post_preview',''	)";
 
 		if ($this->conn->query($sql) === TRUE) {
 		    return 0; // successfull added
